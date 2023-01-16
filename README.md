debb477f8fbf9771bd956e957f670b4430c11e6f
Pavel_Kolotov <pavelkolotov84@gmail.com>
2023-01-16 23:10:24 +0300

    refactoring script

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # Bitlink
 
 ## Описание работы скрипта:
-___
 Скрипт сокращает ссылку через сервис bit.ly, а при введении сокращенной ссылки выводит количество переходов по ней.
 
 ## Зависимости
-___
 Python3:
-
 ```
- pip3 install python-dotenv
- pip3 install requests     
+pip install -r requirements.txt
 ```
 ## Окружение
-___
 .env
 ```
 BITLY_TOKEN=f80a43cd3c1c877df9ce7d7caf24b107df923b7b
+```
+## Запуск скрипта в консоли
+```
+python3 main.py
 ```
--- main.py
+++ main.py
@@ -4,44 +4,60 @@ import requests
 from dotenv import load_dotenv
 from urllib.parse import urljoin, urlparse
 
+
 BITLY_API = 'https://api-ssl.bitly.com/v4/'
-def shorten_link(link):
+
+
+def shorten_link(bitly_token,link):
     payload = {'long_url': link}
-    bitlink_response = requests.post(urljoin(BITLY_API, 'bitlinks'), headers=auth_user, json=payload)
+    auth_header = {'Authorization': f'Bearer {bitly_token}'}
+    bitlink_response = requests.post(
+        urljoin(BITLY_API, 'bitlinks'),
+        headers=auth_header,
+        json=payload
+    )
     bitlink_response.raise_for_status()
-    bitlink = bitlink_response.json()['id']
+    bitlink = bitlink_response.json()['link']
     return bitlink
 
-def count_clicks(bitlink):
-    payload = {'unit': 'month', 'units': '-1'}
-    click_response = requests.get(urljoin(BITLY_API, f'bitlinks/{bitlink}/clicks/summary'), headers=auth_user,
-                                  params=payload)
-    click_response.raise_for_status()
-    user_click = click_response.json()['total_clicks']
-    return user_click
 
-def is_bitlink(parsed_url):
-    is_bitlink_response = requests.get(urljoin(BITLY_API, f'bitlinks/{parsed_url}'), headers=auth_user)
-    return is_bitlink_response.ok
+def count_clicks(bitly_token, bitlink):
+    payload = {'unit': 'month', 'units': '-1'}
+    auth_header = {'Authorization': f'Bearer {bitly_token}'}
+    clicks_response = requests.get(
+        urljoin(BITLY_API, f'bitlinks/{bitlink}/clicks/summary'),
+        headers=auth_header,
+        params=payload
+    )
+    clicks_response.raise_for_status()
+    user_clicks = clicks_response.json()['total_clicks']
+    return user_clicks
+
+
+def is_bitlink(bitly_token, parsed_url):
+    auth_header = {'Authorization': f'Bearer {bitly_token}'}
+    bitlink_response = requests.get(
+        urljoin(BITLY_API, f'bitlinks/{parsed_url}'),
+        headers=auth_header
+    )
+    return bitlink_response.ok
+
+
+def main():
+    bitly_token = os.environ['BITLY_TOKEN']
+    user_url = input('Введите ссылку: ')
+    parsed_url = f'{urlparse(user_url).netloc}{urlparse(user_url).path}'
+    try:
+        if not is_bitlink(bitly_token, parsed_url):
+            bitlink = shorten_link(bitly_token, user_url)
+            print('Битлинк:', bitlink)
+        else:
+            user_clicks = count_clicks(bitly_token, parsed_url)
+            print('По вашей ссылке прошли:', user_clicks)
+    except requests.exceptions.HTTPError:
+        print('Вы ввели неправильную ссылку или указали неверный токен.')
 
 
 if __name__ == "__main__":
     load_dotenv()
-    bitly_token = os.environ['BITLY_TOKEN']
-    auth_user = {'Authorization': f'Bearer {bitly_token}'}
-    user_url = input('Введите ссылку: ')
-    parsed_url = f'{urlparse(user_url).netloc}{urlparse(user_url).path}'
-
-    target = is_bitlink(parsed_url)
-    if not target:
-        try:
-            bitlink = shorten_link(user_url)
-            print('Битлинк:', bitlink)
-        except requests.exceptions.HTTPError:
-            print('Invalid input')
-    else:
-        try:
-            user_click = count_clicks(parsed_url)
-            print('По вашей ссылке прошли:', user_click)
-        except requests.exceptions.HTTPError:
-            print('Invalid input')
+    main()
